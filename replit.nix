49d223421d8b60da425e34e68ef33aa50f06ca89
DimensityDU <77768272+RFS-ADRENO@users.noreply.github.com>
2022-07-29 15:32:56 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,10 +1,10 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-16_x
-    pkgs.libuuid
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.yarn
-    pkgs.replitPackages.jest
+		pkgs.libuuid
+	    	pkgs.nodePackages.typescript-language-server
+    		pkgs.nodePackages.yarn
+    		pkgs.replitPackages.jest
 	];
-  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid]; };
+  	env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid]; };
 }
