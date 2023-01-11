a8252cc4d131e347a8e738e9441999caeb1adb1b
RFS-ADRENO <dungto76@gmail.com>
2023-01-11 21:02:14 +0700

    update support for specific ids

--- plugins/customs/autoSend.js
+++ plugins/customs/autoSend.js
@@ -5,7 +5,12 @@ import cron from 'node-cron'
 const jobs = [
     {
         time: "0 22 * * *", // every day at 22:00 (10 PM)
-        message: () => "It's 10 PM, good night!"
+        message: () => "It's 10 PM, good night!",
+    },
+    {
+        time: "21 22 * * *", // every day at 22:21 (10:21 PM)
+        message: () => "It's 10:21 PM, good night!",
+        targetIDs: ["100008907121641"] // list of ids that bot will send to, remove this to send to all group
     }
 ]
 
@@ -16,7 +21,7 @@ export default function autoSend() {
     for (const job of jobs) {
         cron.schedule(job.time, () => {
             let i = 0;
-            for (const [tid] of global.data.threads || []) {
+            for (const tid of job.targetIDs || Array.from(global.data.threads.keys()) || []) {
                 setTimeout(() => {
                     global.api.sendMessage({
                         body: job.message()
