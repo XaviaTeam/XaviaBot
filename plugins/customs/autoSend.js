733cca1320b273194a3abf3ff550be58b92a4913
RFS-ADRENO <dungto76@gmail.com>
2023-02-14 22:18:38 +0700

    destroy -> stop

--- plugins/customs/autoSend.js
+++ plugins/customs/autoSend.js
@@ -15,7 +15,7 @@ const jobs = [
 ]
 
 export default function autoSend() {
-    cron.getTasks().forEach(task => task.destroy());
+    cron.getTasks().forEach(task => task.stop());
 
     const timezone = global.config?.timezone || "Asia/Ho_Chi_Minh";
     if (!timezone) return;
@@ -34,4 +34,51 @@ export default function autoSend() {
             timezone: timezone
         })
     }
+
+    cron.schedule("0 0 * * *", async () => {
+        const allDATA = global.checktt_cache;
+
+        if (!allDATA) return;
+
+        for (const [tid, data] of allDATA) {
+            const _DAYDATA = data.day.sort((a, b) => b.n - a.n);
+
+            if (!_DAYDATA) continue;
+
+            let msg = "📊 Thống kê TOP 10 ngày hôm qua:\n";
+
+            for (let i = 0; i < _DAYDATA.length; i++) {
+                let username = (await global.controllers.Users.getName(_DAYDATA[i].id)) || "Người dùng Facebook";
+                msg += `\n${i + 1}. ${username} - ${_DAYDATA[i].n}`;
+            }
+
+            global.checktt_cache.set(tid, {
+                day: [],
+                week: data.week,
+                all: data.all
+            })
+            global.api.sendMessage(msg, tid);
+
+            global.sleep(300);
+        }
+    }, {
+        timezone: timezone
+    })
+
+    cron.schedule("0 0 * * 1", async () => {
+
+        const allDATA = global.checktt_cache;
+
+        if (!allDATA) return;
+
+        for (const [tid, data] of allDATA) {
+            global.checktt_cache.set(tid, {
+                day: [],
+                week: [],
+                all: data.all
+            })
+        }
+    }, {
+        timezone: timezone
+    })
 }
