88392b365914f823783bd3a4ae469114e42bfe66
RFS-ADRENO <dungto76@gmail.com>
2024-02-27 20:01:42 +0700

    feat(example): Add balance usages example

--- plugins/commands/example/example_4.js
+++ plugins/commands/example/example_4.js
@@ -15,7 +15,27 @@ const config = {
 };
 
 /** @type {TOnCallCommand} */
-async function onCall({ message }) {
+async function onCall({ message, balance }) {
+    // caller balance
+    balance.add(2000);
+    balance.sub(2000);
+    balance.set(2000);
+
+    balance.get(); // caller money
+
+    // other balance
+    const otherBalance = balance.from("id of other user");
+    if (otherBalance != null) {
+        otherBalance.add(2000);
+        otherBalance.sub(2000);
+        otherBalance.set(2000);
+
+        otherBalance.get(); // other user money
+    }
+
+    balance.make(123n, "200", 233); // Convert all parameters to bigint and calculate their sum. Will throw an error if one is not a valid number
+    balance.makeSafe(123n, false, null); // Similar to balance.make, but returns null instead of throwing an error
+
     // Do something
     message
         .send("React or reply to this message (only command caller)")
@@ -39,12 +59,12 @@ async function onCall({ message }) {
 }
 
 /** @type {TReplyCallback} */
-async function onReply({ message, getLang, data, xDB, eventData }) {
+async function onReply({ message, balance, getLang, data, xDB, eventData }) {
     // do something when someone replied
 }
 
 /** @type {TReactCallback} */
-async function onReaction({ message, getLang, data, xDB, eventData }) {
+async function onReaction({ message, balance, getLang, data, xDB, eventData }) {
     // do something when someone reacted
 }
 
